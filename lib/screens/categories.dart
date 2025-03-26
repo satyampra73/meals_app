0dd1917b48d4afff23f8205bd1c91e1b49e1cd7a
Satyam Prajapati <satyam7355pra@gmail.com>
2025-03-26 14:06:09 +0530

    working on meals show after category click

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(title: 'Some title', meals: []),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pick your category'),
-      ),
+      appBar: AppBar(title: const Text('Pick your category')),
       body: GridView(
         padding: const EdgeInsets.all(25),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
@@ -20,9 +27,13 @@ class CategoriesScreen extends StatelessWidget {
           mainAxisSpacing: 20,
         ),
         children: [
-          for(final Category in availableCategories)
-            CategoryGridItem(category: Category)
-        
+          for (final Category in availableCategories)
+            CategoryGridItem(
+              category: Category,
+              onSelectCategory: () {
+                _selectCategory(context);
+              },
+            ),
         ],
       ),
     );
