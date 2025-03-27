3199ee06d0ec2d078190143621c74673a9fb209f
Satyam Prajapati <satyam7355@gmail.com>
2025-03-27 21:53:05 +0530

    adding meals as favourite and remove part done

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -2,16 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/models/category.dart';
 import 'package:meals_app/screens/meals.dart';
+import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({super.key});
+  const CategoriesScreen({super.key, required this.onToggleFavorite});
+
+  final void Function(Meal meal) onToggleFavorite;
 
   void _selectCategory(BuildContext context,Category category) {
     final filteredMeals = dummyMeals.where((meal) => meal.categories.contains(category.id)).toList();
     Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (ctx) => MealsScreen(title: 'Some title', meals: filteredMeals ),
+        builder: (ctx) => MealsScreen(
+          title: 'Some title', 
+          meals: filteredMeals,  
+          onToggleFavorite: onToggleFavorite,),
       ),
     );
   }
